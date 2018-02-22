3b5ba604c7cd18bf5ddcd30ca2892651c4ca6adf
Kappa Tank <ncq998@gmail.com>
2018-02-22 12:07:35 +0700

    refactor new feed route

--- src/client/app/components/new-feed/new-feed-route.jsx
+++ src/client/app/components/new-feed/new-feed-route.jsx
@@ -2,50 +2,63 @@ import React from "react";
 import {NewFeedLayout} from "../layout/new-feed-layout/new-feed-layout";
 import {PostStuff} from "./post-stuff/post-stuff";
 import {modals} from "../../common/modal/modals";
-let modal;
+import {UserPost} from "./user-post/user-post";
+import {postApi} from "../../../api/ultils-api/post-api";
 
-export class NewFeedRoute extends React.Component{
-    constructor(props){
+
+export class NewFeedRoute extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            expand:false
+        this.state = {
+            expand: false,
+            posts: []
         };
-
+        postApi.getPost().then((posts) => {
+            this.setState({posts}, () => {
+                console.log(this.state.posts);
+            });
+        });
 
     };
-    expandPost=()=>{
-      this.setState({expand:true});
-    };
 
-    render(){
-        let {expand}=this.state;
-
-        if(expand){
-            modal = modals.openModal({
+    render() {
+        let {expand, posts} = this.state;
+        if (expand) {
+            const modal = modals.openModal({
                 content: (
                     <PostStuff
                         expand={expand}
-                        close={()=>modal.close()}
-                        expandPost={()=>this.expandPost()}
+                        close={() => modal.close()}
+                        expandPost={() => this.setState({expand: true})}
                     />
                 ),
                 className: "what-ever"
             });
-            modal.result.then(()=>{
-                this.setState({expand:false});
-            });
+
+            modal.result.then(() => {
+                this.setState({expand: false})
+            })
+
         }
-        return(
+        return (
             <NewFeedLayout>
                 <div className="new-feed-route row justify-content-center">
                     <div className="new-feed col-xl-6 col-lg-7 col-md-8 col-sm-10">
 
-                        {!expand &&
+                        { !expand && (
                             <PostStuff
                                 expand={expand}
-                                expandPost={()=>this.expandPost()}
+                                expandPost={() => this.setState({expand: true})}
                             />
-                        }
+                        )}
+
+                        <div className="new-feed-post">
+                            { posts.map((p, i) => (
+                                <UserPost
+                                    key={i}
+                                />
+                            ))}
+                        </div>
                     </div>
                 </div>
             </NewFeedLayout>
