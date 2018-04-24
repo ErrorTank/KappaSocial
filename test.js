e53987bea390bb11da95bdbd301372df6c9d8993
John Tank <ncq998@gmail.com>
2018-04-24 08:34:50 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 window.addEventListener("click",()=>{
   while(1){
-    alert("oc vcl  ahahahahahahahahhahahahahahhahhahahahah");
-    window.open("https://www.google.com/search?q=mua+bao+cao+su&ie=utf-8&oe=utf-8&client=firefox-b-ab")});
+    console.log("31231231312312312312")
   };
   
   
