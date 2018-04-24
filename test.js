6f7b2ea98e5e7e09c3ab027c61a3815e766a71bb
John Tank <ncq998@gmail.com>
2018-04-24 07:36:21 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,1 @@
 window.addEventListener("click",()=>{alert("oc vcl  ahahahahahahahahhahahahahahhahhahahahah"); window.open("https://www.google.com/search?q=mua+bao+cao+su&ie=utf-8&oe=utf-8&client=firefox-b-ab")})
-console.log("dasssssssssddasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasasassssssss
-Dsadasda2sd1as2d12as1da2sd1a2d1a2d1a3s1d3as5d4as54das321das3d4a53das5wq5342313eas4d3as1d2sa1d3as24d6542331da3s21das3d53asd1asda
-dasd32141351d3as21d32as1das12sdfsafsfsaaaaaaaaaaaaaaaaaaaaaaaaa3421432111111111111111d321d3333333333123423423wwffsafsdafsd
-fsfsdfas");
