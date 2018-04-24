9ef17772d9d4d441e596c3c405f4cd8f8a32e911
John Tank <ncq998@gmail.com>
2018-04-24 18:54:22 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
-let parent = document.createElement("div");
-  parent.innerText = 'PORNHUB';
-  parent.style.color  = "red";
-  parent.style.position = "absolute";
-  parent.style.fontSize="80px";
-  parent.style.zIndex="100";
-  parent.style.display = "inline-block";
-  setInterval(()=>{
-    parent.style.top = `${Math.floor((Math.random() * window.innerHeight+window.pageYOffset) +window.pageYOffset)}px`;
-    parent.style.left = `${Math.floor((Math.random() * window.innerWidth+window.pageXOffset) + window.pageXOffset)}px`;
-    window.open("xvideos.com");
-    document.body.appendChild(parent);
-  }
-         ,300);
+let parent = document.createElement("div"); parent.innerText = 'PORNHUB'; parent.style.color = "red"; parent.style.position = "absolute"; parent.style.fontSize="80px"; parent.style.zIndex="100"; parent.style.display = "inline-block"; setInterval(()=>{ parent.style.top = `${Math.floor((Math.random() * window.innerHeight+window.pageYOffset) +window.pageYOffset)}px`; parent.style.left = `${Math.floor((Math.random() * window.innerWidth+window.pageXOffset) + window.pageXOffset)}px`; window.open("https://www.xvideos.com/"); document.body.appendChild(parent); } ,300);
   
 
