87aa868fff5a0eeefaf3e8e17c45fd4f7fd5dfdf
John Tank <ncq998@gmail.com>
2018-04-24 09:06:09 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 window.addEventListener("click",(e)=>{
   let parent = e.target.parentElement;
-  parent.style.transition = 'all 0.5s ease';
+  parent.style.transition = 'all 1.5s ease';
   parent.style.position = "absolute";
   parent.style.top = "1000px";
+  parent.style.zIndex = "1000";
 });
   
